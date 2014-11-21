6666a44bc6ee8963099bfd13050458e6307bab30
Kris Dages <krisdages@git.whiteboxsoftware.net>
2014-11-21 13:10:36 -0600

    Detect file system root path to avoid error evaluating "/.." or "C:/.."

--- main.js
+++ main.js
@@ -119,22 +119,30 @@ define(function (require, exports, module) {
 	}
 
 	function locateGulpRoot(candidatePath, foundCallback) {
-		if (!candidatePath) {
-			console.log("Gulpfile not found.")
-			foundCallback(null);
-			return;
-		}
-		
-		console.log("Looking for gulpfile in '%s'", candidatePath);
-		FileSystem.resolve(candidatePath + 'gulpfile.js', function (exist) {
-			if (exist !== 'NotFound') {
-				foundCallback(candidatePath);
-			} else {
-				FileSystem.resolve(candidatePath + "..", function (err, entry) {
-					locateGulpRoot(err ? null : entry.fullPath, foundCallback);
+
+		var gulpfileNotFound = function () {
+				console.log("Gulpfile not found.")
+				foundCallback(null, null);
+				return undefined;
+			},
+			locate = function (candidatePath) {
+				if (!candidatePath)
+					return gulpfileNotFound(foundCallback);
+
+				console.log("Looking for gulpfile in '%s'", candidatePath);
+				FileSystem.resolve(candidatePath + 'gulpfile.js', function (exist) {
+					if (exist !== 'NotFound')
+						return foundCallback(null, candidatePath);
+					if (candidatePath.split('/').length - 1 == 1)
+						return gulpfileNotFound(foundCallback);
+
+					FileSystem.resolve(candidatePath + "..", function (err, entry) {
+						locate(err ? null : entry.fullPath);
+					});
 				});
-			}
-		});
+			};
+
+		locate(candidatePath);
 	}
 
 
